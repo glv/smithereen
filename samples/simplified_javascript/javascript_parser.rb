242e8a7355e889f6dadb51388cd0c939979327b0
Glenn Vanderburg <glv@vanderburg.org>
2010-03-08 11:46:59 -0600

    JS parser: eliminated infixr in favor of an :assoc option to infix

--- samples/simplified_javascript/javascript_parser.rb
+++ samples/simplified_javascript/javascript_parser.rb
@@ -27,20 +27,17 @@ module RadishSamples
     end
     protected :module_for_token
   
-    def self.infix_base(type, lbp, rbp, &infix_blk)
+    def self.infix(type, lbp, options={:assoc => :left}, &infix_blk)
+      unless [:left, :right].include?(options[:assoc])
+        raise Radish::GrammarError("Invalid :assoc option: #{options[:assoc]}")
+      end
+      rbp = (options[:associativity] == :left) ? lbp : lbp - 1
+
       tok_module = deftoken(type, lbp)
       infix_blk = lambda{|left| [type, left, expression(rbp)] } unless block_given?
       tok_module.infix &infix_blk
     end
   
-    def self.infix(type, bp, &infix_blk)
-      infix_base(type, bp, bp, &infix_blk)
-    end
-  
-    def self.infixr(type, bp, &infix_blk)
-      infix_base(type, bp, bp-1, &infix_blk)
-    end
-    
     def self.prefix(type, &prefix_blk)
       tok_module = deftoken(type)
       prefix_blk = lambda{ [type, expression(70)] } unless block_given?
@@ -50,10 +47,6 @@ module RadishSamples
     def self.symbol(type, bp=0, &prefix_blk)
       tok_module = deftoken(type, bp)
       tok_module.prefix(&prefix_blk) if block_given?
-    end
-  
-    class <<self
-      protected :infix_base
     end
   
     symbol :':'
@@ -78,6 +71,9 @@ module RadishSamples
       returning(expression(0)) { advance_if_looking_at!(:')') }
     end
     
+    # TODO: while I was working on the other prefix tokens I plugged this in,
+    #       but it depends on scope and statements, and I'd rather tackle those
+    #       with smaller things before getting this to work.
     # prefix :function do
     #   new_scope
     #
@@ -113,33 +109,33 @@ module RadishSamples
       end
     end
     
-    infixr :'&&',  30
-    infixr :'||',  30
+    infix :'&&',  30, :assoc => :right
+    infix :'||',  30, :assoc => :right
 
-    infixr :'===', 40
-    infixr :'!==', 40
-    infixr :'<',   40
-    infixr :'<=',  40
-    infixr :'>',   40
-    infixr :'>=',  40
+    infix :'===', 40, :assoc => :right
+    infix :'!==', 40, :assoc => :right
+    infix :'<',   40, :assoc => :right
+    infix :'<=',  40, :assoc => :right
+    infix :'>',   40, :assoc => :right
+    infix :'>=',  40, :assoc => :right
 
-    infix  :'+',   50
-    infix  :'-',   50
+    infix  :'+',  50
+    infix  :'-',  50
+                  
+    infix  :'*',  60
+    infix  :'/',  60
 
-    infix  :'*',   60
-    infix  :'/',   60
-
-    infix  :'.',   80 do |left|
+    infix  :'.',  80 do |left|
       # ??? Don't really have the concept of arity in our impl.  What's the equivalent?
       raise next_token, "Expected a property name." unless next_token.arity == 'name'
       [:propref, left, take_token]
     end
 
-    infix  :'[',   80 do |left|
+    infix  :'[',  80 do |left|
       returning([:lookup, left, expression(0)]) { advance_if_looking_at! :']' }
     end
 
-    infix  :'(',   80 do |left|
+    infix  :'(',  80 do |left|
       # ??? raise if left is not something callable
       [:call, left, delimited_list(:',', :')'){ expression(0) }]
     end

a4a79c3231eacaab487fa153d05f5752bbd62ffa
Glenn Vanderburg <glv@vanderburg.org>
2011-09-08 09:16:27 -0500

    Added sqrt function to calculator sample.

--- features/calculator.feature
+++ features/calculator.feature
@@ -20,6 +20,7 @@ Feature: Tiny Calculator
       | 24+2*-(3-12)   |     42 |
       | 24+2*-(3-12^2) |    306 |
       | 24+3*4/2 - 5   |     25 |
+      | 6+sqrt(9)      |      9 |
   
   Scenario: Leading whitespace
     When I ask the calculator for the answer to "  2+2"
@@ -38,12 +39,15 @@ Feature: Tiny Calculator
     Then I should get the error "<message>"
     
     Examples:
-      | input | message                               |
-      |       | Unexpected end of input               |
-      | 2+    | Unexpected end of input               |
-      | +2    | Unexpected '+'                        |
-      | 2 2   | Unexpected integer (2)                |
-      | 2+(3  | Unexpected end of input; expected ')' |
-      | 2+)3  | Unexpected ')'                        |
-      | 2+*3  | Unexpected '*'                        |
-      | 2-    | Unexpected end of input               |
+      | input   | message                               |
+      |         | Unexpected end of input               |
+      | 2+      | Unexpected end of input               |
+      | +2      | Unexpected '+'                        |
+      | 2 2     | Unexpected integer (2)                |
+      | 2+(3    | Unexpected end of input; expected ')' |
+      | 2+)3    | Unexpected ')'                        |
+      | 2+*3    | Unexpected '*'                        |
+      | 2-      | Unexpected end of input               |
+      | 3-18(4) | Expected a function name              |
+      | sqrt(8  | Missing closing parenthesis           |
+      | foo(8)  | Unrecognized function                 |
--- samples/calculator.rb
+++ samples/calculator.rb
@@ -12,14 +12,15 @@ module SmithereenSamples
       when /\A\s+/m
         move $&.size
         produce_next_token
-      when /\A\d+/ then make_token(:integer, $&)
-      when /\A\+/  then make_token(:+,       $&)
-      when /\A-/   then make_token(:-,       $&)
-      when /\A\*/  then make_token(:*,       $&)
-      when /\A\//  then make_token(:/,       $&)
-      when /\A\^/  then make_token(:'^',     $&)
-      when /\A\(/  then make_token(:'(',     $&)
-      when /\A\)/  then make_token(:')',     $&)
+      when /\A\d+/          then make_token(:integer, $&)
+      when /\A\+/           then make_token(:+,       $&)
+      when /\A-/            then make_token(:-,       $&)
+      when /\A\*/           then make_token(:*,       $&)
+      when /\A\//           then make_token(:/,       $&)
+      when /\A\^/           then make_token(:'^',     $&)
+      when /\A\(/           then make_token(:'(',     $&)
+      when /\A\)/           then make_token(:')',     $&)
+      when /\A[[:alpha:]]+/ then make_token(:name,    $&)
       end
     end
   end
@@ -31,6 +32,14 @@ module SmithereenSamples
         @value ||= text.to_i
       end
     
+      prefix { value }
+    end
+    
+    deftoken :name, 1000 do
+      def value
+        @value ||= text
+      end
+      
       prefix { value }
     end
   
@@ -55,9 +64,18 @@ module SmithereenSamples
       infix {|left| left ** expression(lbp - 1) }
     end
   
-    deftoken :'(', 0 do
+    deftoken :'(', 50 do
       prefix do
         expression.tap{ advance_if_looking_at! :')' }
+      end
+      infix do |left|
+        raise ::Smithereen::ParseError.new("Expected a function name", left) unless String === left
+        arg = expression(lbp)
+        advance_if_looking_at(:')') or raise ::Smithereen::ParseError.new("Missing closing parenthesis", nil)
+        case left
+        when 'sqrt' then Math.sqrt(arg)
+        else raise ::Smithereen::ParseError.new("Unrecognized function", left)
+        end
       end
     end
   
